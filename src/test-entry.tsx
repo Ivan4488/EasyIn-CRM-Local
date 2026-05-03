8c300b1194b3d9026cd0db138913634408341a24
Ivan <info@petrovdigital.io>
2026-05-02 21:27:53 -0700

    Remove debug/leftover files

--- src/test-entry.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-createRoot(document.getElementById("root")!).render(
-  <div style={{color:"lime",background:"#111",padding:20,fontFamily:"monospace"}}>
-    React is mounting correctly
-  </div>
-);
